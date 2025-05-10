75279c93dc6546398311c2eb5f1df82de0f82770
xiaokuai <116129827+RainyHowCool@users.noreply.github.com>
2025-05-11 07:36:01 +0800

    A better error handler

--- src/parser/exec.rs
+++ src/parser/exec.rs
@@ -1,16 +1,14 @@
 // exec.rs
 // This file will run the outer programs.
 use colored::Colorize;
-use std::os::unix::process::ExitStatusExt;
-use std::process::{Command, ExitStatus};
+use std::process::Command;
 
 pub fn exec(name: String, arg: Vec<String>) {
-    let _ = Command::new(&name).args(arg).status().unwrap_or_else(|_| {
-        println!(
-            "cubesh: {}: Command \"{}\" not found.",
+    let results = Command::new(&name).args(arg).status().unwrap_or_else();
+
+    let _ = match results {
+        Err(_) => println!("cubesh: {}: Command \"{}\" not found.",
             "error".red().bold(),
-            &name
-        );
-        ExitStatus::from_raw(0)
-    });
+            &name),
+        Ok() => todo!();
 }
